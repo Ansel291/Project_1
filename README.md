72ba32eaff46449ef358f079c355680d245fd3fe
Jason Asahara <jasahara@yahoo.com>
2015-10-11 13:13:51 -0700

    designed basic layout, with cards fading in and out, and incorporated sound

---
+++ README.md
@@ -0,0 +1,146 @@
+####User Story:
+
+
+Wanted: Dead or Alive
+
+The concentration memory game
+
+####Score Tracking:
+
+There will be a box that keeps track of player chip count.
+
+The beginning chip count will be $1000.
+
+####Buttons:
+
+Start Button
+
+How to Play/Scoring Button
+
+Bet Button
+
+####As a player/rol
+
+I want to click a start button to start the game
+
+as a player i want to the rules/instructions
+
+
+###Actual User Story:
+
+#####Current
+
+As a player, i want to be able to click on a card and the cards will turn over
+
+As a player, when I click on a second card, and if the card matches the first, two matching cards will remain showing.
+
+As a player, when I click on each card, the card will fade in and out).
+
+As a player, if the second card chosen does not match the first, then the card fades back to it's "wanted" side
+
+
+#####Secondary Priority
+
+As a player, I want to be timed.
+
+When a second player plays, i want the time to be compared to the first player.
+
+As a player, I want to be alerted who has the fastest time
+
+As a player, I want to be notified who won and who lost.
+
+As a player, i want to be able to enter my name so that my name will appear on the screen
+
+
+#####Tertiary Priority
+
+As a player, when i click on each card, there is a gunshot sound.
+
+As a player, when I don't select the two same cards, the cards will turn back over.
+
+As a player, when i click the "play again" i want the 20 cards to be reshuffled
+
+
+###MVP's!
+
+1.  There must be a timer that keeps track of each player
+
+2.  There must be 16 or 20 Cards with 8 to 10 Characters
+
+	* Doc Holiday
+	* Wyatt Earp
+	* Johnny Ringo
+	* Curly Bill Brocious
+	* Madame Mustache
+	* Billy the Kid
+	* Bat Masterson
+	* Bonnie and Clyde
+	* Josephine
+	* Jesse James
+	
+	* Wild Bill Hickok
+	* Big Nose Kate
+	* Butch Cassidy
+	* The Sundance Kid
+	
+	* Pat Garret
+	* Calamity Jane
+	* Annie Oakley
+	
+
+3.  A Winner must be declared
+
+4.  A page with all 20 cards must be designed with buttons with functions that incorporate gunshot sounds
+  
+  
+####Fonts to be Considered:
+  
+  Allerta Stencil
+  
+  Sancreek
+  
+  Gidugu
+  
+<br>
+
+ Smokum  
+
+ Kadwa 
+ 
+ Jacques Francois Shadow
+
+<br>
+ 
+
+Piedra
+   
+Rye
+  
+Poller One
+  
+Vast Shadow
+  
+Diplomata
+     
+Goblin One
+      
+Sree Krushnadevaraya
+   
+<br>  
+  
+Gravitas One
+  
+Holtwood One SC
+  
+Rozha On
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,137 @@
+console.log("active!");
+
+// var memory_array = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L'];
+// var memory_values = [];
+// var memory_tile_ids = [];
+// var tiles_flipped = 0;
+
+// Array.prototype.memory_tile_shuffle = function () {
+// 	var i = this.length, j, temp;
+// 	while (--i > 0) {
+// 		j = Math.floor(Math.random() * (i+1));
+// 		temp = this[j];
+// 		this[j] = this[i];
+// 		this[i] = temp;
+// 	}
+// }
+
+// function newBoard() {
+// 	tiles_flipped = 0;
+// 	var output = '';
+// 	memory_array.memory_tile_shuffle();
+// 	for (var i = 0; i < memory_array.length; i++) {
+// 		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+// 	}
+// 	document.getElementById('memory_board').innerHTML = output;
+// }
+
+console.log("active!");
+
+$('#one').on('click', function() {
+$('.docHollidayA').fadeToggle(700);
+document.querySelector("#noise").play();
+// $('.docHollidayText').fadeToggle(700);
+});
+
+$('#two').on('click', function() {
+$('.josephineA').fadeToggle(700);
+document.querySelector("#noise").play();
+// $('.josephineText').fadeToggle(700);
+});
+
+$('#three').on('click', function() {
+$('.billyTheKidA').fadeToggle(700);
+document.querySelector("#noise").play();
+$('.billyTheKidText').fadeToggle(700);
+});
+
+$('#four').on('click', function() {
+$('.wyattEarpA').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#five').on('click', function() {
+$('.madameMustacheA').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#six').on('click', function() {
+$('.batMastersonA').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#seven').on('click', function() {
+$('.jesseJamesA').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#eight').on('click', function() {
+$('.bonnieAndClydeA').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#nine').on('click', function() {
+$('.buffaloBillA').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#ten').on('click', function() {
+$('.butchCassidyA').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#eleven').on('click', function() {
+$('.butchCassidyB').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#twelve').on('click', function() {
+$('.wyattEarpB').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#thirteen').on('click', function() {
+$('.madameMustacheB').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#fourteen').on('click', function() {
+$('.batMastersonB').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#fifteen').on('click', function() {
+$('.josephineB').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#sixteen').on('click', function() {
+$('.buffaloBillB').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#seventeen').on('click', function() {
+$('.bonnieAndClydeB').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#eighteen').on('click', function() {
+$('.jesseJamesB').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+$('#nineteen').on('click', function() {
+$('.docHollidayB').fadeToggle(700);
+document.querySelector("#noise").play();
+// $('.docHollidayText').fadeToggle(700);
+});
+
+$('#twenty').on('click', function() {
+$('.billyTheKidB').fadeToggle(700);
+document.querySelector("#noise").play();
+});
+
+
+
+
+
