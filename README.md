b3f86f6aa8f13e47bc2039c01b1257bba2e3d3f7
Jason Asahara <jasahara@yahoo.com>
2015-10-14 11:00:35 -0700

    revised version

--- README.md
+++ README.md
@@ -30,13 +30,15 @@ as a player i want to the rules/instructions
 
 #####Current
 
-As a player, i want to be able to click on a card and the cards will turn over
+[ ] As a user, i want to interact with a randomized deck of cards
 
-As a player, when I click on a second card, and if the card matches the first, two matching cards will remain showing.
+[x] As a player, i want to be able to click on a card and the cards will turn over
 
-As a player, when I click on each card, the card will fade in and out).
+[x] As a player, when I click on a second card, and if the card matches the first, two matching cards will remain showing.
 
-As a player, if the second card chosen does not match the first, then the card fades back to it's "wanted" side
+[ ] As a player, when I click on each card, the card will fade in and out).
+
+[ ] As a player, if the second card chosen does not match the first, then the card fades back to it's "wanted" side
 
 
 #####Secondary Priority
@@ -133,6 +135,72 @@ Gravitas One
 Holtwood One SC
   
 Rozha On
+
+
+###Today's Priorities:
+
+Functions:
+
+
+
+Create the intro page with instructions
+
+Welcome to "wanted, dead or alive", the memory game
+
+the object of the game is to select two cards of the same famous American west outlaws/lawmen/women  consecutively with only two chances.  If two of the same card has not been picked, the cards will turn over and you will have to remember where each card was originally.
+
+The player that matches their cards the fastest and clears the entire board in the fastest time wins.
+
+Create the winnner page, showing a picture of wyatt earp and doc holliday winning, and then the loser page with the guy shot
+
+Congratulations player 1, you lived to see another day.
+
+Sorry player 2, your memory was too slow, and you draw your gun fast enough.
+
+
+
+
+
+
+Create a function that keeps track of when 2 cards are flipped, their value is stored and compared, and if the cards match, they remain showing.
+
+Create a function for when all the cards are turned (when the counter turns to 10, the timer stops.
+
+Then the code goes to player 2, and repeats itself
+
+create a function that compares the times of player 1 versus player 2
+
+
+
+
+Create a function that randomizes the cards, and shuffles them, then stores their values
+
+Create function for a timer for player 1
+
+Create a function for a timer for player 2
+
+Create a function that shuffles the cards
+
+
+
+	If player 1 > player 2, player 2 wins
+	
+		else, player 1 wins!
+		
+
+
+
+
+
+1.  Create a Start Button
+
+		When the start button is clicked, it 	
+		activates a timer for player 1
+
+2. Create Timer
+
+		When  
+
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,137 +1,149 @@
-console.log("active!");
+console.log("active!")
 
-// var memory_array = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L'];
-// var memory_values = [];
-// var memory_tile_ids = [];
-// var tiles_flipped = 0;
 
-// Array.prototype.memory_tile_shuffle = function () {
-// 	var i = this.length, j, temp;
-// 	while (--i > 0) {
-// 		j = Math.floor(Math.random() * (i+1));
-// 		temp = this[j];
-// 		this[j] = this[i];
-// 		this[i] = temp;
-// 	}
-// }
+// // Variables
 
-// function newBoard() {
-// 	tiles_flipped = 0;
-// 	var output = '';
-// 	memory_array.memory_tile_shuffle();
-// 	for (var i = 0; i < memory_array.length; i++) {
-// 		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
-// 	}
-// 	document.getElementById('memory_board').innerHTML = output;
-// }
 
-console.log("active!");
-
-$('#one').on('click', function() {
-$('.docHollidayA').fadeToggle(700);
-document.querySelector("#noise").play();
-// $('.docHollidayText').fadeToggle(700);
-});
-
-$('#two').on('click', function() {
-$('.josephineA').fadeToggle(700);
-document.querySelector("#noise").play();
-// $('.josephineText').fadeToggle(700);
-});
-
-$('#three').on('click', function() {
-$('.billyTheKidA').fadeToggle(700);
-document.querySelector("#noise").play();
-$('.billyTheKidText').fadeToggle(700);
-});
-
-$('#four').on('click', function() {
-$('.wyattEarpA').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#five').on('click', function() {
-$('.madameMustacheA').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#six').on('click', function() {
-$('.batMastersonA').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#seven').on('click', function() {
-$('.jesseJamesA').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#eight').on('click', function() {
-$('.bonnieAndClydeA').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#nine').on('click', function() {
-$('.buffaloBillA').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#ten').on('click', function() {
-$('.butchCassidyA').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#eleven').on('click', function() {
-$('.butchCassidyB').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#twelve').on('click', function() {
-$('.wyattEarpB').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#thirteen').on('click', function() {
-$('.madameMustacheB').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#fourteen').on('click', function() {
-$('.batMastersonB').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#fifteen').on('click', function() {
-$('.josephineB').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#sixteen').on('click', function() {
-$('.buffaloBillB').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#seventeen').on('click', function() {
-$('.bonnieAndClydeB').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#eighteen').on('click', function() {
-$('.jesseJamesB').fadeToggle(700);
-document.querySelector("#noise").play();
-});
-
-$('#nineteen').on('click', function() {
-$('.docHollidayB').fadeToggle(700);
-document.querySelector("#noise").play();
-// $('.docHollidayText').fadeToggle(700);
-});
-
-$('#twenty').on('click', function() {
-$('.billyTheKidB').fadeToggle(700);
-document.querySelector("#noise").play();
-});
+var firstchoice;
+var secondchoice;
+var tid;
+var turns = 0;
+var cntr = 0;
+var player = 1;
+var score = 0;
+var backcard = "img/wanted.jpg";
+var isFirstCard = true;
 
 
 
+
+//Array of Names
+
+var faces = new Array(
+ 
+ 'img/docHollidayA.jpg', 
+ 'img/wyattEarpA.jpg', 
+ 'img/billyTheKidA.jpg', 
+ 'img/billyTheKidA.jpg', 
+ 'img/wyattEarpA.jpg', 
+ 'img/docHollidayA.jpg'
+);
+
+var numOfMatches =.5*faces.length; 
+
+
+
+//When player CHOOSES 2 cards
+
+function choose(card) {
+if (turns===2) { 
+	return;
+}
+	if (turns===0) {
+	firstchoice = card;
+  	if (isFirstCard) {
+  		if (player === 1) {
+  			$('#output').text("00:00:00");
+          	$('#output2').text("00:00:00");
+  		}
+  		raceStartTime = new Date;
+  		isFirstCard = false;
+  		startTimer();
+  	}
+  	document.images[card].src = faces[card];
+  	document.querySelector("#noiseone").play();
+  	turns = 1;
+  	} else { 
+  		turns = 2;
+  		secondchoice = card;
+  		document.images[card].src =faces[card];
+  		document.querySelector("#noisetwo").play();
+  		check();
+  		// tid=setInterval("check()",1500);
+   	}
+}
+
+
+
+/*Checking to see if 2 Cards Match, when a match is made, 
+the score increases by 1 point*/
+
+function check() {
+ clearInterval(tid);
+ if (faces[secondchoice]===faces[firstchoice]) {
+     cntr++;
+     score++;
+     if (cntr == numOfMatches) {
+    	// startPause();
+    		clearInterval(raceTimer);
+			alert("Congrats player " + player + "!  You made " + score + " matches!!!");
+          	isFirstCard = true;
+          	score = 0;
+          	(player === 1 ? player =2 : player =1); 
+          	var choice;
+       //    document.images[firstchoice].src = backcard;
+    	  // document.images[secondchoice].src = backcard;
+       		cntr = 0;
+       		newBoard();
+       		shuffle();
+         }
+     	turns = 0;
+      return ;
+    }
+  else {
+    document.images[firstchoice].src = backcard;
+    document.images[secondchoice].src = backcard;
+    turns = 0;
+    return ;
+       }
+}
+
+
+
+// Shuffles the Cards
+
+function shuffle() {
+var holder;
+var swaps;
+var i;
+var j;
+	for (swaps=0; swaps<12; swaps++) {
+ 		i = Math.floor(Math.random()*faces.length);
+ 		j = Math.floor(Math.random()*faces.length);
+ 			holder = faces[i];
+ 			faces[i] = faces[j];
+ 			faces[j]=holder;
+  	}
+}
+
+
+
+//Turns over cards and creates new board
+
+function newBoard() {
+	for (var i = 0; i < faces.length; i++) {
+		document.images[i].src = backcard;
+	}
+}
+
+
+
+//Function for starting and stopping 
+
+
+
+var raceTimer;
+var raceStartTime = new Date;
+var startTimer;
+
+startTimer = function () {
+raceTimer = setInterval(function() {
+		raceEndTime = new Date;
+		if (player === 1) {
+		$('#output').text(((raceEndTime - raceStartTime)/1000).toFixed(1) + " Seconds"); } else {
+			$('#output2').text(((raceEndTime - raceStartTime)/1000).toFixed(1) + " Seconds");}
+		}, 100);
+	
+}
 
 
